d92e0908076bb9ac7e538a02c180773879ada6ca
Andy <1303680675@qq.com>
2024-05-12 10:47:52 +0800

    Update main.cpp

--- 5/5-7/main.cpp
+++ 5/5-7/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     qDebug() << "/*                                    QList                                   */";
     qDebug() << "/* -------------------------------------------------------------------------- */";
     // QList 类
-    qDebug() << "QList 类";
+    qDebug() << "1.QList 类";
     QList<int> qlist; // 初始化一个空的 QList<int>列表
     for (int i = 0; i < 10; i++)
         qlist.insert(qlist.end(), i + 10);
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 
     // 通过 QList<int>::iterator 读写迭代器
     qDebug() << endl;
-    qDebug() << "通过 QList<int>::iterator 读写迭代器";
+    qDebug() << "2.通过 QList<int>::iterator 读写迭代器";
     QList<int>::iterator x;
     qDebug() << "Result1:";
     for (x = qlist.begin(); x != qlist.end(); x++)
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 
     // 初始化一个 QList<int>const_iterator 只读迭代器
     qDebug() << endl;
-    qDebug() << "初始化一个 QList<int>const_iterator 只读迭代器";
+    qDebug() << "3.初始化一个 QList<int>const_iterator 只读迭代器";
     qDebug() << "Result1:";
     QList<int>::const_iterator qciter;
     // 输出列表所有的值
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 
     // 向 qlist 添加元素
     qDebug() << endl;
-    qDebug() << "向 qlist 添加元素";
+    qDebug() << "4.向 qlist 添加元素";
     qlist.append(666);
     QList<int>::iterator itr1;
     qDebug() << "Result2:";
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
 
     // 查询 qlist 当中元素
     qDebug() << endl;
-    qDebug() << "查询 qlist 当中元素";
+    qDebug() << "5.查询 qlist 当中元素";
     qDebug() << "Result3:";
     qDebug() << qlist.at(3);
     qDebug() << qlist.contains(77);
@@ -57,14 +57,14 @@ int main(int argc, char *argv[])
 
     // 修改 qlist 列表里面的元素值
     qDebug() << endl;
-    qDebug() << "修改 qlist 列表里面的元素值";
+    qDebug() << "6.修改 qlist 列表里面的元素值";
     qDebug() << "Result4:";
     qlist.replace(5, 888);
     qDebug() << qlist;
 
     // 删除元素
     qDebug() << endl;
-    qDebug() << "删除元素";
+    qDebug() << "7.删除元素";
     qDebug() << "Result5:";
     qlist.removeAt(0);
     qlist.removeFirst();
@@ -77,20 +77,20 @@ int main(int argc, char *argv[])
     qDebug() << "/* -------------------------------------------------------------------------- */";
 
     // QLinkedList 类
-    qDebug() << "初始化 QLinkedList 类";
+    qDebug() << "1.初始化 QLinkedList 类";
     QLinkedList<QString> qAllMonth;
     for (int i = 1; i <= 12; i++)
         qAllMonth << QString("%1%2").arg("Month:").arg(i);
 
     // 读写迭代器
-    qDebug() << "读写迭代器";
+    qDebug() << "2.读写迭代器";
     qDebug() << "Result1:";
     QLinkedList<QString>::iterator itrw = qAllMonth.begin();
     for (; itrw != qAllMonth.end(); itrw++)
         qDebug() << *itrw;
     // 只读迭代器
     qDebug() << endl;
-    qDebug() << "只读迭代器";
+    qDebug() << "3.只读迭代器";
     qDebug() << "Result2:";
     QLinkedList<QString>::const_iterator itr = qAllMonth.constBegin();
     for (; itr != qAllMonth.constEnd(); itr++)
