7227b76d89209ed07bad324bf90da771e97bb293
Andy <1303680675@qq.com>
2024-06-06 11:28:19 +0800

    Adding comments

--- 8/8-1/02_QPainterProssss/painterarea.h
+++ 8/8-1/02_QPainterProssss/painterarea.h
@@ -23,6 +23,13 @@ public:
     void setShape(shape);  // 此函数实现绘制形状
     void setPen(QPen);
     void setBrush(QBrush);
+    /* 
+    函数 void setShape(shape);, void setPen(QPen);, void setBrush(QBrush); 的作用：
+    void setShape(shape);: 这个函数是 PainterArea 类的一个公有成员函数，用于设置绘图区域的形状。
+    void setPen(QPen);: 这个函数是 PainterArea 类的一个公有成员函数，用于设置绘图区域绘制形状时所使用的画笔（QPen）。
+    void setBrush(QBrush);: 这个函数是 PainterArea 类的一个公有成员函数，用于设置绘图区域绘制形状时所使用的画刷（QBrush）。
+    这三个函数的作用都是改变 PainterArea 类对象的属性，从而影响绘图区域的绘制结果。通过调用这些函数，可以动态地改变绘图区域的形状、画笔和画刷，使得绘图区域能够以不同的方式呈现出用户所期望的图形。
+    */
 
     void paintEvent(QPaintEvent *); // 重绘事件
     void setfillrule(Qt::FillRule); // 填充规则
--- 8/8-1/02_QPainterProssss/widget.cpp
+++ 8/8-1/02_QPainterProssss/widget.cpp
@@ -44,3 +44,24 @@ void Widget::dispShapeFunc(int v)
     PainterArea::shape sp=PainterArea::shape(comboboxshape->itemData(v,Qt::UserRole).toInt());
     labelshapeArea->setShape(sp);
 }
+/* 
+    1 
+    函数void Widget::dispShapeFunc(int v) 的解释：
+    这个函数是在一个名为 Widget 的类中定义的。函数接受一个整数参数 v。
+    1.1
+    它的目的是根据传入的整数值来设置 PainterArea 类的形状（shape）。
+    1.2
+    函数内部通过 comboboxshape 的 itemData 方法获取对应下拉框中选项的用户自定义数据，
+    转换为整数，并转换为 PainterArea 类中的 shape 枚举类型。然后，通过调用 PainterArea 类的 setShape 方法，
+    将得到的形状值传递给 PainterArea 对象的实例 labelshapeArea，从而改变绘图区域的形状。
+
+    2
+    在上述代码中，Qt::UserRole 被用于获取下拉框（comboboxshape）中每个选项的自定义数据。
+    具体来说，itemData(v, Qt::UserRole) 方法用于检索下拉框中第 v 个选项关联的用户自定义数据，
+    然后通过 toInt() 方法将其转换为整数。
+
+    Qt::UserRole 的作用是允许开发人员将自定义的数据与Qt框架提供的控件（如下拉框）中的每个选项相关联。
+    这样，开发人员可以存储任何他们想要的数据，而不仅仅是显示文本。
+    在这种情况下，Qt::UserRole 被用于存储与每个下拉框选项相关联的形状（PainterArea::shape），
+    并且在 Widget::dispShapeFunc 函数中用于动态设置 PainterArea 对象的形状。
+ */
